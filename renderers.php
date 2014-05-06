4d26ef168442299528117ddb10e14121824ba02d
Cathal O'Riordan <coriordan@wit.ie>
2014-05-06 10:49:11 +0100

    Removed. Replaced with 'simple' course format.

--- renderers.php
+++ renderers.php
@@ -30,4 +30,3 @@ require_once('renderers/course_renderer.php');
 require_once('renderers/files_renderer.php');
 require_once('renderers/block_settings_renderer.php');
 require_once('renderers/enrol_renderer.php');
-require_once('renderers/course_format_renderer.php');
