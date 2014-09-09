62c20f928b7b10afac8c9195f1b4328837070720
Cathal O'Riordan <coriordan@wit.ie>
2014-09-09 16:59:49 +0100

    bumped release version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014090800;
+$plugin->version   = 2014090900;
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v0.1.5';
+$plugin->release   = 'v0.1.6';
 $plugin->component = 'theme_edgy';
