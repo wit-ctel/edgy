8d9a9d6e5f00b999b4de8848bd4b9ce4324dec60
Cathal O'Riordan <coriordan@wit.ie>
2014-12-19 12:58:23 +0000

    bumped version number

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014131100;
+$plugin->version   = 2014192400;
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v0.2.2';
+$plugin->release   = 'v0.2.3';
 $plugin->component = 'theme_edgy';
