a7a9d06521890ba328248598a0c6ed978669758f
Cathal O'Riordan <coriordan@wit.ie>
2015-01-14 15:34:26 +0000

    It's 2015!

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014140100;
+$plugin->version   = 2015140100;
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v0.2.4';
+$plugin->release   = 'v0.2.5';
 $plugin->component = 'theme_edgy';
