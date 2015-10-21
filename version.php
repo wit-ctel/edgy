a0cbec710ffb2b8081c91433a131a983dbb06455
Cathal O'Riordan <cathal@madein75.com>
2015-10-21 10:39:09 +0100

    version 0.3.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2015090800; // YYYYMMDDRR.XX
+$plugin->version   = 2015102100; // YYYYMMDDRR.XX
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v0.2.9';
+$plugin->release   = 'v0.3.0';
 $plugin->component = 'theme_edgy';
